b7145a5a8ed78df8827d072855035791e63afc7c
lewis-kori <lewiskosh@gmail.com>
2020-08-29 16:30:18 +0300

    index page styling

---
+++ components/HomePage/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="services">
+    <div class="container text-white">
+      <h1 class="text-uppercase text-center">What we do</h1>
+      <div class="row text-center">
+        <div class="col-md-4 services">
+          <img src="/manager.svg" class="service-img" />
+          <h4>Multi tenancy</h4>
+          <p>
+            Manage multiple organization data all in one platform.
+          </p>
+        </div>
+        <div class="col-md-4 services">
+          <img src="/finance_app.svg" class="service-img" />
+          <h4>Expense tracking</h4>
+          <p>
+            Budget for each department and track individual expenses.
+          </p>
+        </div>
+        <div class="col-md-4 services">
+          <img src="/motivation.svg" class="service-img" />
+          <h4>Financial Literacy</h4>
+          <p>
+            Educate yourself more on finance to achieve financial freedom.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
+  <div class="container">
     <Jumbotron />
+    <About />
   </div>
 </template>
 
 <script>
+import About from '../components/HomePage/About'
 import Jumbotron from '../components/HomePage/Jumbotron'
 export default {
-  components: { Jumbotron },
+  components: { About, Jumbotron },
 }
 </script>
